5eb087818cfc5f1b996a1c1495fa0e767cb3fec8
James Benjamin <james.abenj@gmail.com>
2020-03-07 21:46:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     else
-      resp.write "Path Not Found"
+      resp.write "Your cart is empty"
     end
 
     if req.path.match(/cart/)
