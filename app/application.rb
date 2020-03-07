427b1d30beb926b09af9caa78fc1893a61b19786
James Benjamin <james.abenj@gmail.com>
2020-03-07 21:43:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,18 @@ class Application
       resp.write "Path Not Found"
     end
 
+    if req.path.match(/cart/)
+      @@items.each do |cart|
+        resp.write "#{cart}\n"
+      end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    else
+      resp.write "Path Not Found"
+    end
+    
+
     resp.finish
   end
 
