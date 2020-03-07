4080cbfb7e1c5b43d6917f04b0c11e03f43bd412
James Benjamin <james.abenj@gmail.com>
2020-03-07 22:48:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,7 @@ class Application
     end
 
     if req.path.match(/add/)
-      search_term = req.params[item]
+      search_term = req.params["item"]
       if @@items.include?(search_term)
         @@cart << search_term
         resp.write "added #{search_term}"
